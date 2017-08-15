3f7403200bff26f94a69c17b2e66cb1a2ea56f81
Vlad Busov <to2002to2002@gmail.com>
2017-08-15 18:19:06 +0300

    Create party.py

---
+++ party.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+print("Стоит ли идти тебе на вечеринку?")
+
+vodka = float(input("Есть ли водка?\n"))
+rain = float(input("Есть ли дождик?\n"))
+friend = float(input("Есть ли твой лучший друг?\n"))
+
+def activation_function(x):
+    if x >=0.5:
+        return 1
+    else:
+        return 0
+
+def predict(vodka, rain, friend):
+    inputs = np.array([vodka, rain, friend])
+    weights_input_to_hiden_1 = [0.25, 0.25 , 0]
+    weights_input_to_hiden_2 = [0.5, -0.4, 0.9]
+    weights_input_to_hiden = np.array([weights_input_to_hiden_1,weights_input_to_hiden_2])
+
+    weights_hiden_to_output = np.array([-1, 1])
+
+    hiden_input = np.dot(weights_input_to_hiden, inputs)
+    print("hiden_input: " + str(hiden_input))
+
+    hiden_output = np.array([activation_function(x) for x in hiden_input])
+    print("hiden_output: " + str(hiden_output))
+
+    output = np.dot(weights_hiden_to_output, hiden_output)
+    print("output: " + str(output))
+    return activation_function(output) == 1
+
+print("result: " + str(predict(vodka, rain, friend)))
